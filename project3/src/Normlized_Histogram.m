e269a21d5b1ee58a0c366f9c003244c685c5ecf7
Michael Andersen <bp@bp-laptop.(none)>
2010-03-13 14:44:57 +0100

    Added some pictures and NormLizedHistogram

--- project3/src/Normlized_Histogram.m
+++ project3/src/Normlized_Histogram.m
@@ -1,5 +1,5 @@
 function [ xout n ] = Normlized_Histogram( data, bins )
-%UNTITLED Summary of this function goes here
+%Normalized_Histogram Summary of this function goes here
 %   Detailed explanation goes here
 
 [n xout] = hist(data, bins);
@@ -9,8 +9,6 @@ bin_width = abs(xout(1) - xout(2)); % Bin width
 area = sum(n)*bin_width;
 
 n = n / area;
-
-bar(xout, n, 'BarWidth', 1)
 
 end
 
